414db8acf4c38a4af6a75e15e0428ed44d41762c
unknown <pavithralakshmi84@gmail.com>
2025-11-22 21:15:08 +0530

    update

---
+++ React1/src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from '../src/component/navbar.jsx'
+import "./App.css"
+import NavBar1 from './component/banner.jsx' 
+
+
+const App = () => {
+  return (
+   <>
+    <NavBar/>
+    <NavBar1/>
+   </>
+  )
+}
+
+export default App
---
+++ React1/src/component/banner.jsx
@@ -0,0 +1,42 @@
+const NavBar1=()=>{
+    return(
+        <>
+        <div className="main">
+        <div className="content">
+          <div>
+            <h1>Atlas Database</h1>
+            <p>The most effective way to deploy MongoDB. Enjoy unmatched resilience, scalability, and enterprise-grade security while eliminating operational complexity.
+            </p>
+            <div className="content">
+              <button>Get Started</button>
+              <a href="">Contact Sales </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="image">
+          <img src="https://images.contentstack.io/v3/assets/blt7151619cb9560896/blt4b8f5d88e7e5fcb7/6570d7ff19f33769508d1cf6/General_TECHNOLOGY_Database_Spot_BS_Mist.svg" alt="" />
+        </div>
+      </div>
+      <div className="main">
+        <div>
+          <img src="https://images.contentstack.io/v3/assets/blt7151619cb9560896/blt557771e610c05b3a/672281e0fae3bb3a466be5d0/Slalom_-_3_2_(4).svg" alt="" />
+        </div>
+        <div className="content1">
+          <div>
+            <h1>AI Ready</h1>
+            <p>MongoDB enables seamless integration with AI technologies. Harness the power of retrieval-augmented generation (RAG) by utilizing vector search for innovative data handling. Effortlessly build AI-powered apps that scale with your needs, ensuring efficiency and performance.
+            </p>
+            <div className="content1">
+        <a href="">Build AI-Powered Apps</a>
+      </div>
+          </div>
+        </div>
+        </div>
+    
+   
+        
+        </>
+    )
+}
+export default NavBar1;
